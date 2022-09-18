bec6a47a7bf854c70deccb849ddef1fcc470f84e
SoggySaussages <vmdmaharaj@gmail.com>
2022-09-17 21:42:34 -0700

    Listener first test

---
+++ common/listeners/devtest/devtest.go
@@ -0,0 +1,18 @@
+package devtestlistener
+
+import (
+	"net/http"
+)
+
+func StartListening() {
+
+	type HttpHandler struct{}
+
+	func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+		data := []byte("Devtest Query returned")
+		res.Write(data)
+	}
+
+	handler := HttpHandler{}
+	http.ListenAndServe(":9000", handler)
+}
