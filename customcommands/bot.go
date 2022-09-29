89d683a5134533462ef4a1a4582ed823753c7eec
SoggySaussages <vmdmaharaj@gmail.com>
2022-09-28 21:55:24 -0700

    rand fix v2

--- customcommands/bot.go
+++ customcommands/bot.go
@@ -758,7 +758,7 @@ func ExecuteCustomCommand(cmd *models.CustomCommand, tmplCtx *templates.Context,
 	}
 
 	if debugErr {
-		errCase := (899999)+100000
+		errCase := rand.Intn(899999)+100000
 		if slashtrigger {
 			out = fmt.Sprintf("**Case Number:** `%d`\n%s", errCase, out)
 			return fmt.Sprintf("An error occurred. This has been logged. Check <#1023064061824479242> or <#1019413814548123678> for support.\n`Case number: %d`", errCase), nil
