9e3421dfa0ee9221fc3e4ae9835dd3a099cf0d4c
SoggySaussages <vmdmaharaj@gmail.com>
2022-09-28 20:54:13 -0700

    putting it on hold

--- stdcommands/devtestlistener/devtestlistener.go
+++ stdcommands/devtestlistener/devtestlistener.go
@@ -1,9 +1,9 @@
 package devtestlistener
 
 import (
-	"net/http"
-	"io"
-	"fmt"
+//	"net/http"
+//	"io"
+//	"fmt"
 
 	"github.com/botlabs-gg/yagpdb/v2/commands"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
@@ -21,17 +21,17 @@ var Command = &commands.YAGCommand{
 	},
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
 
-	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:9000/%d/%s", data.Args[0].Int64(), data.Args[1].Str())
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	
-	if string(body) = "none" {
-		return "No entry found", err
-	}
+//	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:9000/%d/%s", data.Args[0].Int64(), data.Args[1].Str())
+//	if err != nil {
+//		return nil, err
+//	}
+//	defer resp.Body.Close()
+//	body, err := io.ReadAll(resp.Body)
+//	
+//	if string(body) = "none" {
+//		return "No entry found", err
+//	}
 
-	return string(body), err
-
+//	return string(body), err
+return "this command is on hold for a sec, i'll work on it closer to testing environment", err
 	}}
