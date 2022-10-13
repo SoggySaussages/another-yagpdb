4872b1c0092fafe5010ff2b63b333bb3ced0e58f
SoggySaussages <vmdmaharaj@gmail.com>
2022-10-12 21:12:39 -0700

    see what happens if we try to build

--- common/templates/context_funcs.go
+++ common/templates/context_funcs.go
@@ -16,7 +16,7 @@ import (
 	"github.com/botlabs-gg/yagpdb/v2/common/scheduledevents2"
 	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dstate"
-	"github.com/botlabs-gg/yagpdb/v2/lib/template"
+//	"github.com/botlabs-gg/yagpdb/v2/lib/template"
 )
 
 var ErrTooManyCalls = errors.New("too many calls to this function")
@@ -1867,7 +1867,7 @@ func (c *Context) forceError(print string) (string, error) {
 
 func (c *Context) getLocation() (string, error) {
 //	s := s.retState()
-	location, _ := s.tmpl.ErrorContext(s.node)
-	return location, nil
+//	location, _ := s.tmpl.ErrorContext(s.node)
+//	return location, nil
 return "deprecated", nil
 }
