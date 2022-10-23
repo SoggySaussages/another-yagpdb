87e6df125dad13c7c9c2856f081b1574fd546c65
SoggySaussages <vmdmaharaj@gmail.com>
2022-10-22 21:19:06 -0700

    autocorrect :pQuack:

--- lib/discordgo/interactions.go
+++ lib/discordgo/interactions.go
@@ -525,7 +525,7 @@ type InteractionResponseData struct {
 	AllowedMentions *AllowedMentions   `json:"allowed_mentions,omitempty"`
 	Flags           uint64             `json:"flags,omitempty"`
 	Files           []*File            `json:"-"`
-	File 			*file				`json:"-"`
+	File 			*File				`json:"-"`
 
 	// NOTE: autocomplete interaction only.
 	Choices []*ApplicationCommandOptionChoice `json:"choices,omitempty"`
