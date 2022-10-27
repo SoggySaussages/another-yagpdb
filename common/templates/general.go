a578284bea981b7f39b5c5068ed1536fa468ecc6
SoggySaussages <vmdmaharaj@gmail.com>
2022-10-26 22:35:18 -0700

    using logrus

--- common/templates/general.go
+++ common/templates/general.go
@@ -630,7 +630,7 @@ func CreateMessageSend(values ...interface{}) (*discordgo.MessageSend, error) {
 }
 
 func CreateInteractionResponseSend(values ...interface{}) error {
-	log.Print("Interaction Response doing it")
+	logrus.Debug("Interaction Response doing it")
 	if len(values) < 1 {
 		return nil
 	}
@@ -720,26 +720,26 @@ func CreateInteractionResponseSend(values ...interface{}) error {
 				Reader: &buf,
 				Name: fmt.Sprint("attachment_", time.Now().Format("2006-01-02_15-04-05")),
 			}
-			log.Print("File doing it")
+			logrus.Debug("File doing it")
 
 		case "filename":
 			// Cut the filename to a reasonable length if it's too long
 			file.Name = common.CutStringShort(ToString(val), 64)
-			log.Print("Filename doing it")
+			logrus.Debug("Filename doing it")
 		default:
 			return errors.New(`invalid key "` + key + `" passed to send message builder`)
 		}
 
 	}
 	if file != nil {
-		log.Print("File true")
+		logrus.Debug("File true")
 		// We hardcode the extension to .png because we're sending a png :)
 		// data.File.Name = filename // + ".png"
 
 		data.Attachments = append(data.Attachments, file)
 	}
 
-	log.Print("Sending")
+	logrus.Debug("Sending")
 	common.BotSession.InteractionExecuteComplex(id, token, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: data,
