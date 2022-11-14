386384f21442fb034099f8b1404f8036459445c3
SoggySaussages <vmdmaharaj@gmail.com>
2022-11-13 17:04:25 -0800

    testing embed fix

--- common/templates/general.go
+++ common/templates/general.go
@@ -1174,6 +1174,7 @@ func CreateMessageEdit(values ...interface{}) (*discordgo.MessageEdit, error) {
 			msg.Content = &temp
 		case "embed":
 			if val == nil {
+				msg.Embeds = []*discordgo.MessageEmbed{}
 				continue
 			}
 			v, _ := indirect(reflect.ValueOf(val))
