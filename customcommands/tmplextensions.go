f8ad95f2ad5d5b63ca420fdf4e4ff2327d0d25a6
SoggySaussages <110698921+SoggySaussages@users.noreply.github.com>
2022-12-07 20:22:49 -0800

    Revert "syntax"
    This reverts commit 7db3d50c83ca6894320d6bc244b035e372a086f2.

--- customcommands/tmplextensions.go
+++ customcommands/tmplextensions.go
@@ -228,7 +228,7 @@ func tmplRunCC(ctx *templates.Context) interface{} {
 			Message: ctx.Msg,
 
 			IsExecedByLeaveMessage: ctx.IsExecedByLeaveMessage,
-			ImmediateStackDepth: currentStackDepth,
+			ImmediateStackDepth: currentStackDepth
 		}
 
 		// embed data using msgpack to include type information
